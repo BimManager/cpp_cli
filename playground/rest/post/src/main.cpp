e1724e886a641eac4426096ceb41fb92c74fe4ac
bimManager <kirillkozlov@ymail.com>
2019-09-26 09:46:13 +0300

    doing

---
+++ playground/rest/post/src/main.cpp
@@ -0,0 +1,13 @@
+// Copyright 2019 Samolet LLC
+// Author: kkozlov
+// main.cpp
+
+#include <stdio.h>
+
+#using <System.dll>
+
+int	main(int argc, char **argv)
+{
+  printf("Fooo");
+  return (0);
+}
