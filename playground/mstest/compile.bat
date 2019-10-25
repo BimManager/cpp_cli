93ef32dd463b43ae4e72e18cbaf0c69f56cc7bc9
bimManager <kirillkozlov@ymail.com>
2019-10-25 15:21:26 +0300

    a

--- playground/mstest/compile.bat
+++ playground/mstest/compile.bat
@@ -1,2 +1,4 @@
 cl /LD /clr bank.cpp bank_unittests.cpp /AI"c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\ReferenceAssemblies\v2.0" 
 mstest /testcontainer:bank.dll 
+
+vcvars32.bat 
